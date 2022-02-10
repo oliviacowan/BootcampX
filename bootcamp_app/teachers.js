7ef3b77aca5d3d42404094b90c63eed84c19b619
Olivia <olivia.cowan@hotmail.com>
2022-02-10 01:16:05 +0000

    node postgres and database connection

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const { Pool } = require ('pg');
+
+const args = process.argv;
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${args[2] || JUL02}'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+})
+.catch(err => console.log('query error: ', err.stack));
